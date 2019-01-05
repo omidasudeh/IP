827906123d40090c42018c38cb4822f828754afb
omid <omid.asudeh@gmail.com>
2019-01-05 15:11:18 -0500

    valid-number

---
+++ __Algorithm/0. Basic_Algorithms/0.2. Sorting/mergeSort/mergeSort.cpp
@@ -0,0 +1,138 @@
+/*
+Implements randomized MergeSort and its testing
+ - Recursive:
+    - Space Complexity: best: average: worst:O(n) heap + log(n) stack
+    - Time Complexity : best: average: worst: O(nlogn)
+*/
+#include <gtest/gtest.h>
+#include <iostream>
+#include <vector>
+#include "../quickSort/quickSort.cpp"
+using namespace std;
+// =============================== Implementation ===========================
+void Merge(vector<int>&array, int left,int right){
+    if (array.size()<1)
+        __throw_length_error("empty input array!");
+    if(left<right){
+        vector<int> temp(right-left+1);
+        int mid = (left+right)/2;
+        int i = left, j=mid+1;
+        int k = 0;
+        while(i<mid+1 && j<right+1){
+            if(array[i]<=array[j]){
+                temp[k] = array[i];
+                i++;
+                k++;
+            }
+            else{
+                temp[k] = array[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<mid+1){
+            temp[k] = array[i];
+            i++;
+            k++;
+        }
+        while(j<right+1){
+            temp[k] = array[j];
+            j++;
+            k++;
+        }
+        for(int k = 0;k<temp.size();k++)
+            array[left+k] = temp[k];
+    }
+    else if(left == right) return; 
+    else
+        __throw_invalid_argument("left should be >= right");
+
+}
+void MyMergeSort(vector<int>& array, int left, int right){
+    if(left<right){ 
+        int mid = (left+right)/2;
+        MyMergeSort(array,left, mid);
+        MyMergeSort(array,mid+1,right);
+        Merge(array,left,right);
+    }
+    else if(left == right) return; 
+    else
+        __throw_invalid_argument("left should be >= right");
+}
+void MyMergeSort(vector<int>& array)
+{
+    if (array.size()<1)
+        __throw_length_error("empty input array!");
+    MyMergeSort(array,0,array.size()-1);
+}
+// =============================== testing ==================================
+TEST(MergeSort, Merge_emptyArray){
+    //expect length exception if array is empty
+    vector<int> arr;
+    ASSERT_THROW(Merge(arr,0,10), std::length_error);
+}
+TEST(MergeSort, Merge_oneElementArray){
+    //expect sort of one element equal itself
+    vector<int> arr = {1};
+    Merge(arr,0,0);
+    ASSERT_EQ(arr[0], 1);
+}
+TEST(MergeSort, Merge_incorrectLeftRightArgs){
+    //expect invalid_args exception if wrong boundaries
+    vector<int> arr = {13,6,20};
+    ASSERT_THROW(Merge(arr,2,0), std::invalid_argument);
+}
+TEST(MergeSort, Merge_working){
+    // 1. Given an even sized array with both halves sorted, merge the first half to the second
+    // 2. Sort another copy of the input with quick sort
+    // 3. expect result of 1. equal to 2.
+    // 4. do the same for odd sized array    
+    vector<int> arrEven = {13,15,22,33,35,44,1,2,3,4,5,6};
+    vector<int> arrEven1 = {13,15,22,33,35,44,1,2,3,4,5,6};
+    Merge(arrEven,0,arrEven.size()-1);
+    myQSort(arrEven1);
+    for(int i = 0;i<arrEven.size();i++){
+        ASSERT_EQ(arrEven[i],arrEven1[i]);
+    }
+
+    vector<int> arrOdd = {13,15,22,33,35,44,1,2,3,4,5};
+    vector<int> arrOdd1 = {13,15,22,33,35,44,1,2,3,4,5};
+    Merge(arrOdd,0,arrOdd.size()-1);
+    myQSort(arrOdd1);
+    for(int i = 0;i<arrOdd.size();i++){
+        ASSERT_EQ(arrOdd[i],arrOdd1[i]);
+    }
+}
+TEST(MergeSort, emptyArray){
+    //expect length exception if empty array
+    vector<int> arr;
+    ASSERT_THROW(MyMergeSort(arr), std::length_error);
+}
+TEST(MergeSort, oneElementArray){
+    //expect sort of one element equal itself
+    vector<int> arr = {1};
+    MyMergeSort(arr);
+    ASSERT_EQ(arr[0],1);
+}
+TEST(MergeSort, working){
+    // expect the array to be sorted
+    // 1. sort a given even sized array usin mergesort
+    // 2. Sort another copy of the input with quick sort
+    // 3. expect result of 1. equal to 2.
+    // 4. do the same for odd sized array    
+    vector<int> arrEven  = {2,4,3,7,4,-6,5,89,2,4,5,55,6,78};
+    vector<int> arrEven1 = {2,4,3,7,4,-6,5,89,2,4,5,55,6,78};
+    MyMergeSort(arrEven);
+    myQSort(arrEven1);
+    for(int i = 0;i<arrEven.size();i++){
+        ASSERT_EQ(arrEven[i],arrEven1[i]);
+    }
+
+    vector<int> arrOdd  = {2,4,3,7,4,-6,5,89,4,5,55,6,78};
+    vector<int> arrOdd1 = {2,4,3,7,4,-6,5,89,4,5,55,6,78};
+    MyMergeSort(arrOdd);
+    myQSort(arrOdd1);
+    for(int i = 0;i<arrOdd.size();i++){
+        ASSERT_EQ(arrOdd[i],arrOdd1[i]);
+    }
+}
--- __Coding/Problems/valid-number.cpp
+++ __Coding/Problems/valid-number.cpp
@@ -1,112 +1,91 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+#include <gtest/gtest.h>
+using namespace std;
+// Another possible solution is using regex. Here we assume we don't have regex
 class Solution {
 public:
+    string trim(string s){
+        int i = 0, j = s.size()-1;
+        while(s[i]==' ')i++;
+        while(s[j]==' ')j--;
+        return s.substr(i,j-i+1);
+    }
     bool isNumber(string s) {
-        s = stringToString(s);
-        char first_char = s[0];
-        if(first_char != '+' && first_char !='-' && !digit(first_char))//if first char is not sign or digit return false
-            return false;
-        if(first_char == '+' || first_char =='-') //if first char is sign
-        {
-            string t(s,1,s.size()-1);
-            s = t;
-        }
-        
-        int dot_position = has_dot(s);
-        string dec = "";
-        if(dot_position!=-1)//if number has dot
-        {
-            string t (s,0,dot_position);
-            dec = t;
-            if(!number(dec)) 
-                return false;
-            string t1 (s,dot_position+1,s.size()-dot_position-1);
-            s = t1;
-            
-            string frac ="";
-            int exp_pos = has_exp(s);
-            if(exp_pos!=-1)//if has exponent
-            {
-                string t (s,0,exp_pos);
-                frac = t;
-                if(!number(frac)) 
-                    return false;
-                string exp (s,exp_pos+1,s.size()-exp_pos-1);
-                if(!number(exp)) 
-                    return false;
-            }
-        }
-        else if(has_exp(s)!=-1)//if has exponent
-        {
-            int exp_pos = has_exp(s);            
-            string t (s,0,exp_pos);
-            dec = t;
-            if(!number(dec)) 
-                return false;
-            string exp (s,exp_pos+1,s.size()-exp_pos-1);
-            if(!number(exp)) 
-                return false;
-        }
-        else
-        {
-            dec = s;
-            if(!number(dec)) 
-                return false;
-        }
-        return true;
-
-    }
-    int has_exp(string s)
-    {
-        for(int i=0;i<s.size();i++)
-        {
-            char ch = s[i];
-            if(ch =='e')
-                return i;
-        }
-        return -1;
-    }
-    int has_dot(string s)
-    {
-        for(int i=0;i<s.size();i++)
-        {
-            char ch = s[i];
-            if(ch =='.')
-                return i;
-        }
-        return -1;
-    }
-    bool digit(char ch)
-    {
-        return (ch>57 || ch < 48)?false:true;
-    }
-    bool number(string s)
-    {
         if(s.size()==0)
             return false;
-        for(char ch:s)
-            if(!digit(ch)) //if char is not digit return false
+        s = trim(s);
+        size_t ePos = s.find('e');
+        if(string::npos != ePos){// if dot found
+            string base = s.substr(0,ePos);
+            string exponent = s.substr(ePos+1);
+            return (decimalNumber(base)&&integer(exponent));    
+        }
+        return (decimalNumber(s));   
+    }
+    bool decimalNumber(string s){
+        if(s.size()==0)
+            return false;
+        size_t dotPos = s.find('.');
+        if(string::npos != dotPos){// if dot found
+            string dec = s.substr(0,dotPos);
+            string frac = s.substr(dotPos+1);
+            if(dec =="" && frac == "") return false;
+            if(integer(dec)&& frac == "") return true;
+            if((dec ==""||dec =="+"||dec =="-") && num(frac)) return true;
+            if(integer(dec) && num(frac)) return true;
+        }
+        return (integer(s));    
+    }
+    bool integer(string s){// pattern: [+/-][d+]
+        if(s.size()==0)
+            return false;
+        if(s[0] == '-' || s[0] == '+')// if has sign
+            s = s.substr(1);//extract number
+        return num(s);
+    }
+    bool num(string s){
+        if(s.size()==0)
+            return false;
+        for(char ch:s)// check for all digit
+            if(!(ch >= '0' && ch <= '9')) //if char is not digit return false
                 return false;
         return true;
     }
-    string stringToString(string s) {
-        if(s[0]==' '&& s[s.size()-1]==' ')
-        {
-            string t (s,1,s.size()-2);
-                return t;
-        }
-        else if(s[0]==' '&& s[s.size()-1]!=' ')
-        {
-            string t (s,1,s.size()-1);
-                return t;
-        }
-        else if(s[0]!=' '&& s[s.size()-1]==' ')
-        {
-            string t (s,0,s.size()-1);
-                return t;
-        }
-        else 
-            return s;
-        
-}
 };
+//================================== TEST ================================
+class testIsNumber :public testing::Test{
+    public:
+        Solution sol;
+};
+TEST_F(testIsNumber,trim){
+    ASSERT_EQ(sol.trim("   omid    "),"omid");
+}
+TEST_F(testIsNumber,falseCases){
+    ASSERT_FALSE(sol.isNumber("abc"));
+    ASSERT_FALSE(sol.isNumber("1 a"));
+    ASSERT_FALSE(sol.isNumber(" 1e"));
+    ASSERT_FALSE(sol.isNumber("e3"));
+    ASSERT_FALSE(sol.isNumber(" 99e2.5 "));//?
+    ASSERT_FALSE(sol.isNumber(" --6 "));
+    ASSERT_FALSE(sol.isNumber("-+3"));
+    ASSERT_FALSE(sol.isNumber("95a54e53"));
+    ASSERT_FALSE(sol.isNumber("."));
+    ASSERT_FALSE(sol.isNumber("1 ."));
+    ASSERT_FALSE(sol.isNumber(".-4"));//!
 
+}
+TEST_F(testIsNumber,trueCases){
+    ASSERT_TRUE(sol.isNumber("0"));
+    ASSERT_TRUE(sol.isNumber(".1"));
+    ASSERT_TRUE(sol.isNumber("3."));
+    ASSERT_TRUE(sol.isNumber(" 0.1 "));
+    ASSERT_TRUE(sol.isNumber("2e10" ));
+    ASSERT_TRUE(sol.isNumber(" -90e3   "));
+    ASSERT_TRUE(sol.isNumber(" 6e-1"));
+    ASSERT_TRUE(sol.isNumber("53.5e93"));
+    ASSERT_TRUE(sol.isNumber("+.8"));//!
+
+}

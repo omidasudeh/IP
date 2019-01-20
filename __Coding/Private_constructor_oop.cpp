bb97a831dc352496117f60356c7e506972f93be8
omid <omid.asudeh@gmail.com>
2019-01-19 21:29:01 -0500

    private_constructor

--- __Coding/Private_constructor_oop.cpp
+++ __Coding/Private_constructor_oop.cpp
@@ -1,3 +1,8 @@
+/* 
+- this is to demonstrate restricting constructor for all but the friends
+  * in this example we restrict the constructor in the form of base(int v) to friends only;
+  * but we allow the default constructor for all 
+*/
 #include <iostream>
 using namespace std;
 class base{
@@ -5,6 +10,7 @@ class base{
         int val;
         base(int v):val(v){;} // friend class can instantiate from base
     public:
+        base(){;} // here default constructor is not restricted
         void print(){
             cout<<"val is: "<<val<<endl;
         }
@@ -19,6 +25,7 @@ class myfriend{
 };
 int main(){
     // base b(12); // private constructor prohibits instantiation except fot friends
+    base b;
     myfriend mf;
     return 0;
 }
